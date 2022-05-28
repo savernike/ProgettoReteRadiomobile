b731273e5897973e2f8cefa691b46825e8600354
savernike <mcrsrn99l13h224c@studenti.unirc.it>
2022-05-28 19:10:46 +0200

    Cambiamenti nel main

--- main.m
+++ main.m
@@ -1,10 +1,12 @@
 clear all
 clc
 
-algorithm= [1 2 3]; %1=CMS,2=OMS, 3=MS
+algorithm= [1 2 3]; %1=CMS,2=OMS,3=MS
 
-Nsim=150; %Simulations Numbers
+Nsim=150; %Numero di simulazioni
+Tsim = 50; %Tempo di simulazione [ms]
 
 NUE= 250; %Users 
 
 FR=[1]; %Frequency range 1 o 2 (3GPP TS 38.104)
+
